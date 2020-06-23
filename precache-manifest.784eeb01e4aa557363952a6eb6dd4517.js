ef475e2a002ecf4336df561174d2ab30b51cadff
Varada <varada.iyengar@pt-corp.com>
2020-06-22 20:35:30 -0500

    Updates

---
+++ precache-manifest.784eeb01e4aa557363952a6eb6dd4517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a4c805a8c624792cff869fff547c5a",
+    "url": "/rasamfriends/index.html"
+  },
+  {
+    "revision": "642fa148efd12714273c",
+    "url": "/rasamfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fae40ad6961b3127cf2c",
+    "url": "/rasamfriends/static/css/main.60711a31.chunk.css"
+  },
+  {
+    "revision": "642fa148efd12714273c",
+    "url": "/rasamfriends/static/js/2.62f1875a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rasamfriends/static/js/2.62f1875a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae40ad6961b3127cf2c",
+    "url": "/rasamfriends/static/js/main.b73d6bad.chunk.js"
+  },
+  {
+    "revision": "ba0a25849a9289fe5794",
+    "url": "/rasamfriends/static/js/runtime-main.50c5a696.js"
+  }
+]);
